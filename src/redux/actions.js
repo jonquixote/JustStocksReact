289fa6c719cca146e6be6c19df7fed0b45629a4b
“jonuixote” <jehawley@gmail.com>
2020-04-17 18:31:30 -0700

    heroku

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -16,18 +16,6 @@ export const MENU_CHANGE_DEFAULT_CLASSES = "MENU_CHANGE_DEFAULT_CLASSES";
 export const MENU_CHANGE_HAS_SUB_ITEM_STATUS =
   "MENU_CHANGE_HAS_SUB_ITEM_STATUS";
 
-/* TODOAPP*/
-export const TODO_GET_LIST = "TODO_GET_LIST";
-export const TODO_GET_LIST_SUCCESS = "TODO_GET_LIST_SUCCESS";
-export const TODO_GET_LIST_ERROR = "TODO_GET_LIST_ERROR";
-export const TODO_GET_LIST_WITH_FILTER = "TODO_GET_LIST_WITH_FILTER";
-export const TODO_GET_LIST_WITH_ORDER = "TODO_GET_LIST_WITH_ORDER";
-export const TODO_GET_LIST_SEARCH = "TODO_GET_LIST_SEARCH";
-export const TODO_ADD_ITEM = "TODO_ADD_ITEM";
-export const TODO_ADD_ITEM_SUCCESS = "TODO_ADD_ITEM_SUCCESS";
-export const TODO_ADD_ITEM_ERROR = "TODO_ADD_ITEM_ERROR";
-export const TODO_SELECTED_ITEMS_CHANGE = "TODO_SELECTED_ITEMS_CHANGE";
-
 /* CHAT APP*/
 export const CHAT_GET_CONTACTS = "CHAT_GET_CONTACTS";
 export const CHAT_GET_CONTACTS_SUCCESS = "CHAT_GET_CONTACTS_SUCCESS";
@@ -66,7 +54,6 @@ export const SURVEY_SAVE = "SURVEY_SAVE";
 export * from "./menu/actions";
 export * from "./settings/actions";
 export * from "./auth/actions";
-export * from "./todo/actions";
 export * from "./chat/actions";
 export * from "./surveyList/actions";
 export * from "./surveyDetail/actions";
