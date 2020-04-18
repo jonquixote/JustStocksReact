33b1aa54248c4e6f3deb41deb18146f4b53bbe99
“jonuixote” <jehawley@gmail.com>
2020-04-17 17:30:12 -0700

    heroku

--- craco.config.js
+++ craco.config.js
@@ -6,6 +6,7 @@ module.exports = {
       alias: {
         "@app": path.resolve(__dirname, "src/"),
         Redux: path.resolve(__dirname, ‘src/redux/’)
+        "todo/reducer": path.resolve(__dirname, ‘src/redux/todo/reducer’)
       }
     }    
   }
