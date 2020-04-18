dd584deca49c1c692d7ab1b7f830c1af3c1d4cc2
“jonuixote” <jehawley@gmail.com>
2020-04-17 18:28:14 -0700

    heroku

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -2,7 +2,6 @@ import { combineReducers } from 'redux';
 import settings from './settings/reducer';
 import menu from './menu/reducer';
 import authUser from './auth/reducer';
-import todoApp from './todo/reducer';
 import chatApp from './chat/reducer';
 import surveyListApp from './surveyList/reducer';
 import surveyDetailApp from './surveyDetail/reducer';
@@ -11,7 +10,6 @@ const reducers = combineReducers({
   menu,
   settings,
   authUser,
-  todoApp,
   chatApp,
   surveyListApp,
   surveyDetailApp
--- src/redux/sagas.js
+++ src/redux/sagas.js
@@ -1,6 +1,5 @@
 import { all } from 'redux-saga/effects';
 import authSagas from './auth/saga';
-import todoSagas from './todo/saga';
 import chatSagas from './chat/saga';
 import surveyListSagas from './surveyList/saga';
 import surveyDetailSagas from './surveyDetail/saga';
@@ -8,7 +7,6 @@ import surveyDetailSagas from './surveyDetail/saga';
 export default function* rootSaga(getState) {
   yield all([
     authSagas(),
-    todoSagas(),
     chatSagas(),
     surveyListSagas(),
     surveyDetailSagas()
