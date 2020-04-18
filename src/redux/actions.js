90dc8fb8e91fb40c1589390ca1137b6beefcce9e
“jonuixote” <jehawley@gmail.com>
2020-04-17 17:57:20 -0700

    react-create-app on Heroku

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -66,7 +66,6 @@ export const SURVEY_SAVE = "SURVEY_SAVE";
 export * from "./menu/actions";
 export * from "./settings/actions";
 export * from "./auth/actions";
-export * from "./todo/actions";
 export * from "./chat/actions";
 export * from "./surveyList/actions";
 export * from "./surveyDetail/actions";
